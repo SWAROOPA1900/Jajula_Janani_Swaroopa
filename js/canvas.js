f6b4a35186de02ce108dfa1bb806acfecf820d3d
Jajula Janani Swaroopa <61306585+SWAROOPA1900@users.noreply.github.com>
2022-03-14 14:26:25 +0530

    Update canvas.js

--- js/canvas.js
+++ js/canvas.js
@@ -6,7 +6,7 @@ canvas.height = window.innerHeight;
 
 var stars = [], // Array that contains the stars
   FPS = 30, // Frames per second
-  x = 170, // Number of stars
+  x = 150, // Number of stars
   mouse = {
     x: 0,
     y: 0
